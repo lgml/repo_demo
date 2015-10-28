7fe368a9f9c7784aacdbf4048711a5c55a3f171d
lluna <lluna@tcpsi.es>
2015-10-28 16:13:53 +0100

    Mensaje descriptivo

---
+++ lab_git/src/com/tcpsi/es/formacion/lab_git_main.java
@@ -0,0 +1,11 @@
+package com.tcpsi.es.formacion;
+
+public class lab_git_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este es un ejemplo de uso de Git");
+		System.out.println("Este es un ejemplo de uso de Git");
+	}
+
+}
